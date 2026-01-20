99aa9c0293904e41527ec26bff9350bea6a80885
Divya_Ganechar <divyaganechar@gmail.com>
2026-01-20 20:45:50 +0530

    Add files via upload

---
+++ dsa16bubblesort.cpp
@@ -0,0 +1,39 @@
+// program to implement bubble sort
+#include <iostream>
+using namespace std;
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap if elements are in wrong order
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to print array elements
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = 7;
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);  // Sort the array
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
